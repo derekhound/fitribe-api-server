1b17f20696d17f25f34d0aa13fb41d7128902aef
derekhound <derekhound@gmail.com>
2015-12-06 07:29:16 +0800

    Extend built-in User model as user model

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,6 +1,6 @@
 var server = require('./server');
 var ds = server.dataSources.mysql;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+var lbTables = ['user', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
 ds.automigrate(lbTables, function(err) {
   if (err) throw err;
   console.log('Looback tables [' + lbTables + '] created in ', ds.adapter.name);
